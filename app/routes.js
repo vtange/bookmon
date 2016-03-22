2b3b0eb075606be678194ddda018a63362336192
Victor Tang <victortang1992@gmail.com>
2016-03-22 12:19:33 -0700

    don't need game pages routes

--- app/routes.js
+++ app/routes.js
@@ -13,22 +13,5 @@ module.exports = function(app) {
 			packagedUser : JSON.stringify([req.user]) //send user info to angular
         }); // load the index.ejs file
     });
-	// =====================================
-    // GAME PAGES ========
-    // =====================================
-    app.get('/game/*', function(req, res) {
-		var filename = req.originalUrl.replace("/game/","");
-		//if it has a dot in its name return the original file
-		if (/(\.)/g.test(req.originalUrl)){
-			res.sendfile(appRoot + '/public/' + filename);
-		}
-		else{
-			res.render('index.ejs', {
-				title : app.title, // get the title
-				user : req.user, // get the user out of session and pass to template
-				packagedUser : JSON.stringify([req.user]) //send user info to angular
-			}); // load the index.ejs file
-		}
-    });
 
 };
