569e3691ba88c6f6684e14cce939567a0c54fce2
Victor Tang <victortang1992@gmail.com>
2016-04-04 15:32:33 -0700

    fix stats being much higher than they should

--- public/services.js
+++ public/services.js
@@ -55,31 +55,41 @@ body
 						//book traits
 					case "Adventure":
 						stats.story += 20;
+						break;
 					case "Historical":
 						stats.story += 10;
 						stats.knowledge += 10;
+						break;
 					case "Comedy":
 						stats.story += 10;
 						stats.speed += 10;
+						break;
 					case "Romance":
 						stats.story += 10;
 						stats.magic += 10;
+						break;
 					case "Sci Fi":
 						stats.knowledge += 10;
 						stats.magic += 10;
+						break;
 					case "Fantasy":
 						stats.magic += 20;
+						break;
 					case "Math-heavy":
 						stats.knowledge += 20;
+						break;
 					case "Mundane":
 						stats.knowledge += 20;
 						stats.speed -= 10;
 						stats.story -= 10;
+						break;
 					case "Light":
 						stats.speed += 20;
 						stats.weight -= 10;
+						break;
 					case "Hardcover":
 						stats.weight += 20;
+						break;
 						// book bodies
 					case "Comic":
 						stats.weight += 20;
@@ -87,36 +97,42 @@ body
 						stats.knowledge += 20;
 						stats.story += 40;
 						stats.speed += 40;
+						break;
 					case "Novel":
 						stats.weight += 30;
 						stats.magic += 30;
 						stats.knowledge += 30;
 						stats.story += 40;
 						stats.speed += 30;
+						break;
 					case "Journal":
 						stats.weight += 30;
 						stats.magic += 20;
 						stats.knowledge += 50;
 						stats.story += 40;
 						stats.speed += 20;
+						break;
 					case "Textbook":
 						stats.weight += 40;
 						stats.magic += 30;
 						stats.knowledge += 50;
 						stats.story += 20;
 						stats.speed += 20;
+						break;
 					case "Artbook":
 						stats.weight += 40;
 						stats.magic += 50;
 						stats.knowledge += 20;
 						stats.story += 20;
 						stats.speed += 30;
+						break;
 					case "Scripture":
 						stats.weight += 40;
 						stats.magic += 50;
 						stats.knowledge += 10;
 						stats.story += 50;
 						stats.speed += 10;
+						break;
 				}
 			});
 			return stats;
