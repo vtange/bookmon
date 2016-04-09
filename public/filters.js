92d19c2faa425f76f7b25b6b89fef491939da5a0
Victor Tang <victortang1992@gmail.com>
2016-04-09 15:52:08 -0700

    show own trades

--- public/filters.js
+++ public/filters.js
@@ -9,3 +9,14 @@ angular.module('bookmonGame').filter('noSelfTrade', ['ActiveUser', function(Acti
         return out;
     }
 }]);
+angular.module('bookmonGame').filter('SelfTrade', ['ActiveUser', function(ActiveUser) {
+    return function(input) {
+        var out = [];
+        for (var j = 0; j < input.length; j++) {
+			if(input[j].who._id === ActiveUser.user._id){
+				out.push(input[j]);
+			};
+        }
+        return out;
+    }
+}]);
